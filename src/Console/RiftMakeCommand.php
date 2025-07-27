76230beaa33c408c76092104bff3c5296833b310
singlephon <singlephon@gmail.com>
2025-07-28 02:24:41 +0500

    Fix class generator

--- src/Console/RiftMakeCommand.php
+++ src/Console/RiftMakeCommand.php
@@ -73,6 +73,41 @@ class RiftMakeCommand extends MakeCommand
         }
     }
 
+    protected function createClass($force = false, $inline = false)
+    {
+        $classPath = $this->parser->classPath();
+
+        if (File::exists($classPath) && ! $force) {
+            $this->line("<options=bold,reverse;fg=red> WHOOPS-IE-TOOTLES </> 😳 \n");
+            $this->line("<fg=red;options=bold>Class already exists:</> {$this->parser->relativeClassPath()}");
+
+            return false;
+        }
+
+        $this->ensureDirectoryExists($classPath);
+
+        File::put($classPath, $this->classContents());
+
+        return $classPath;
+    }
+
+    public function classContents()
+    {
+        $stubName = 'rift.stub';
+
+        if (File::exists($stubPath = __DIR__ . '/../../stubs/' .$stubName)) {
+            $template = file_get_contents($stubPath);
+        } else {
+            $template = file_get_contents(__DIR__.DIRECTORY_SEPARATOR.$stubName);
+        }
+
+        return preg_replace(
+            ['/\[namespace\]/', '/\[class\]/', '/\[view\]/'],
+            [$this->parser->classNamespace(), $this->parser->className(), $this->parser->viewName()],
+            $template
+        );
+    }
+
     protected function createJs($force = false, $inline = false): string
     {
         if ($inline) {
